ac359e5dbc73e504f8c3ad8bcb290b4c004dd21f
Dylan Phelan <dtphelan1@gmail.com>
2020-02-27 16:25:30 -0500

    Simulator code works:

--- sim.py
+++ sim.py
@@ -14,18 +14,25 @@ POPCAP = 10000
 # Error margin for floats
 EPSILON = 0.001
 # Percentage of population that breeds at every step
-BREED_RATE = 0.01
+BREED_RATE = 0.001
 
 class HeterozygousTraitSimulator:
   def __init__(self):
-    self.population = []
-    self.populationRecord = []
     self.population_cap = POPCAP
     self.ep = EPSILON
     self.breed_rate = BREED_RATE
+    self.zero_population()
+
+  def zero_population(self):
+    print("Zero population")
+    self.population = []
+    self.population_record = []
 
   # Initialize the population based on initial percentages
   def init_population(self, hetero_zyg, homo_zyg_dom, homo_zyg_rec):
+    print("init pop")
+    # Zero population
+    self.zero_population()
     # Want to ensure the percentage_sum is within bounds
     percentage_sum = (hetero_zyg + homo_zyg_dom + homo_zyg_rec)
     if percentage_sum < (1.0 - self.ep) or percentage_sum > 1.0 + self.ep:
@@ -43,6 +50,7 @@ class HeterozygousTraitSimulator:
   # Given the current population, cull by a certain amount;
   # If no amount provided, ensure it is below the cap
   def cull_population(self, amount=None):
+    print("cull_population")
     cur_size = len(self.population)
     # If amount is defined, cull by that amount if it's less than the current  size
     if (amount is not None) and (amount < cur_size):
@@ -59,10 +67,12 @@ class HeterozygousTraitSimulator:
 
   # Based on the current population, reproduce and simulate a new population
   def simulate_new_pop(self):
-    new_offspring = []
+    print("simulate new pop")
+    new_offspring_arr = []
     cur_size = len(self.population)
     offspring_needed = int(self.breed_rate * cur_size)
-    while (len(new_offspring) < offspring_needed):
+    print("making how many new offspring? " + str(offspring_needed))
+    while (len(new_offspring_arr) < offspring_needed):
       # Select two parents
       parent_1 = random.choice(self.population)
       parent_2 = random.choice(self.population)
@@ -70,20 +80,21 @@ class HeterozygousTraitSimulator:
       trait_1 = parent_1.select_allele()
       trait_2 = parent_2.select_allele()
       offspring = PopulationMember(t1=trait_1, t2=trait_2)
-      new_offspring.append(new_offspring)
+      new_offspring_arr.append(offspring)
     # Once offspring are finished, add them to the population and then cull
-    self.cull_population(len(new_offspring))
-    self.population += new_offspring
+    self.cull_population(len(new_offspring_arr))
+    self.population += new_offspring_arr
 
   # Articulate statistics on the current population
   def population_stats(self, run, percentage_done):
+    print("recording pop stats")
     return {
       "run": run,
       "percentage_done": percentage_done,
       "pop_size": len(self.population),
-      "hetero": len([member for member in self.population if member.trait_label() == HETERO_LABEL]) / len(self.population),
-      "homo_d": len([member for member in self.population if member.trait_label() == HOMO_D_LABEL]) / len(self.population),
-      "homo_r": len([member for member in self.population if member.trait_label() == HOMO_R_LABEL]) / len(self.population)
+      "hetero": len([member for member in self.population if member.get_trait_label() == HETERO_LABEL]) / len(self.population),
+      "homo_d": len([member for member in self.population if member.get_trait_label() == HOMO_D_LABEL]) / len(self.population),
+      "homo_r": len([member for member in self.population if member.get_trait_label() == HOMO_R_LABEL]) / len(self.population)
     }
 
   # Run a simulation `number_runs` long with initial population distributed along the
@@ -91,29 +102,36 @@ class HeterozygousTraitSimulator:
     # init the population
     self.init_population(hetero_zyg, homo_zyg_dom, homo_zyg_rec)
     # run the simulation
-    curRun = 0
+    curRun = 1
     tenth_of_run = number_runs / 10
     print(tenth_of_run)
-    while curRun < number_runs:
+    while curRun <= number_runs:
       # Every 10th of the way through, log the states
       if curRun % tenth_of_run == 0:
         percentage_done = (curRun / tenth_of_run) * 10
         print(percentage_done)
         cur_stats = self.population_stats(curRun, percentage_done)
-        self.populationRecord.append(cur_stats)
+        self.population_record.append(cur_stats)
       self.simulate_new_pop()
       curRun += 1
 
+  def get_population_stats(self):
+    return self.population_record
 
 class PopulationMember():
-  def __init__(self, trait_label, t1=None, t2=None):
+  def __init__(self, trait_label=None, t1=None, t2=None):
     # If the traits are defined directly, use them
-    self.traits = [t1, t2]
-    self._set_label_from_traits(self.traits)
+    if t1 is not None and t2 is not None:
+      self.traits = [t1, t2]
+      self._set_label_from_traits(self.traits)
 
     # If the trait label is set, use that directly
-    self.trait_label = trait_label
-    self._set_traits_from_label(self.trait_label)
+    if trait_label is not None:
+      self.trait_label = trait_label
+      self._set_traits_from_label(self.trait_label)
+
+    if self.trait_label is None:
+      raise Exception("COULDN'T BUILD POP MEMBER WELL")
 
   # Given a trait_label, determine the traits
   def _set_traits_from_label(self, trait_label):
@@ -143,7 +161,7 @@ class PopulationMember():
     else:
       raise Exception("Unknown traits - " + ", ".join(traits))
 
-  def trait_label():
+  def get_trait_label(self):
     return self.trait_label
 
   def select_allele(self):
@@ -162,3 +180,4 @@ if __name__ == "__main__":
     homo_zyg_rec,
     number_runs
   )
+  print(simulator.get_population_stats())
